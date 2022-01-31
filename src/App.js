eddf9eea87789931b7a86e6903669fc2caef7a24
A Nunez <anunez2102@gmail.com>
2022-01-31 17:08:17 -0500

    added hobby search button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
-
 import React from 'react';
 
+let location = document.getElementById("test")
+
 const App = () => {
+  // if (navigator.geolocation) {
+  //   navigator.geolocation.getCurrentPosition(showPosition);
+  // } else {
+  //   location.innerHTML = "Geolocation not supported"
+  // }
+
   return (
-    <div>
-      
+    <div id='buttons'>
+      <p>
+        <label>Enter your desired hobby search by starting the search with 'How to'
+          <input name="query" type="text" pattern="How to" placeholder="how to..." aria-label="search" size="8" /><button id="search-button" onclick="App()">Search</button>
+        </label>
+      </p>
     </div>
+
+    
   );
 }
+
+function showPosition(position) {
+  location.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
+
 
 export default App
