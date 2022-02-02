ed17a2e12ae0f4a9b2c9611469ad2afed121889a
A Nunez <anunez2102@gmail.com>
2022-02-01 21:10:36 -0500

    adding components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import './App.css';
 import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom'
 
-let location = document.getElementById("test")
+
 
 const App = () => {
-  // if (navigator.geolocation) {
-  //   navigator.geolocation.getCurrentPosition(showPosition);
-  // } else {
-  //   location.innerHTML = "Geolocation not supported"
-  // }
-
   return (
-    <div id='buttons'>
-      <p>
-        <label>Enter your desired hobby search by starting the search with 'How to'
-          <input name="query" type="text" pattern="How to" placeholder="how to..." aria-label="search" size="8" /><button id="search-button" onclick="App()">Search</button>
-        </label>
-      </p>
-    </div>
+    <Router>
+      <Navbar/>
+    </Router>
 
-    
-  );
-}
-
-function showPosition(position) {
-  location.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-}
-
+//search bar, need to add API info
+    // <div id='buttons'>
+    //   <p>
+    //     <label>Enter your desired hobby search by starting the search with 'How to'
+    //       <input name="query" type="text" pattern="How to" placeholder="how to..." aria-label="search" size="8" /><button id="search-button" onclick="App()">Search</button>
+    //     </label>
+    //   </p>
+    // </div>
+   )
+   }
+  
 
 export default App
